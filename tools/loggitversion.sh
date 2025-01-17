ff322025c12e2c0e5e4d4b9fe892d255746fb1a2
RikyPlaza <rikyrod2001@gmail.com>
2025-01-17 04:49:26 -0500

    tools/loggitversion.sh: implemented first basic logic of branch detection
    Signed-off-by: RikyPlaza <rikyrod2001@gmail.com>

--- tools/loggitversion.sh
+++ tools/loggitversion.sh
@@ -3,6 +3,9 @@
 UVG_BRANCH=`git branch --show-current`
 
 if [ x"${UVG_BRANCH}" = "main" ] ; then
+	UVG_TAG=`git describe --tags --abbrev=0`
+	echo "${UVG_TAG}"
 else
 	LAST_COMMIT_ID=`git log --format="%h" -n 1`
+	echo "${LAST_COMMIT_ID}"
 fi
