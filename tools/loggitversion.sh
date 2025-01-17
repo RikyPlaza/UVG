63a8f7bedb7be24b6b53648165885b3b0fd51b0d
RikyPlaza <rikyrod2001@gmail.com>
2025-01-17 05:24:31 -0500

    tools/loggitversion.sh: implemented versioning on master branch
    Signed-off-by: RikyPlaza <rikyrod2001@gmail.com>

--- tools/loggitversion.sh
+++ tools/loggitversion.sh
@@ -4,9 +4,17 @@ UVG_BRANCH=`git branch --show-current`
 
 if [ "${UVG_BRANCH}" = "main" ] ; then
 	UVG_TAG=`git describe --tags --abbrev=0`
-	echo "${UVG_TAG}"
+	COMMIT_NUMBER=`git rev-list  `git rev-list --tags --no-walk --max-count=1`..HEAD --count`
+	LAST_COMMIT_ID=`git log --format="%h" -n 1`
+
+	if [ "${COMMIT_NUMBER}" = "0" ] ; then
+		UVG_PACKAGE_VERSION="${UVG_TAG}"
+	else
+		UVG_PACKAGE_VERSION="${UVG_TAG}"."${COMMIT_NUMBER}"-"${LAST_COMMIT_ID}"
+	fi
 else
 	LAST_COMMIT_ID=`git log --format="%h" -n 1`
 	UVG_PACKAGE_VERSION="${UVG_BRANCH}"-"${LAST_COMMIT_ID}"
-	echo "${UVG_PACKAGE_VERSION}"
 fi
+
+echo "${UVG_PACKAGE_VERSION}"
