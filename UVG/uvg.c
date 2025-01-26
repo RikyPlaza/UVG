f52185b22536fb3f02cb9f20ec4c1606f1170588
RikyPlaza <rikyrod2001@gmail.com>
2025-01-26 10:57:27 +0000

    UVG/uvg.cpp: added basic xml read for testing purpose.
    Signed-off-by: RikyPlaza <rikyrod2001@gmail.com>

---
+++ UVG/uvg.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <libxml/xmlmemory.h>
+#include <libxml/parser.h>
+
+void getReference(xmlDocPtr doc, xmlNodePtr cur) {
+
+	cur = cur->xmlChildrenNode;
+	while (cur != NULL) {
+	    if ((!xmlStrcmp(cur->name, (const xmlChar *)"reference"))) {
+		    printf("uri: %s\n", xmlGetProp(cur, "uri"));
+		    }
+	    cur = cur->next;
+	}
+    return;
+}
+
+
+void parseDoc(char *docname) {
+
+	xmlDocPtr doc;
+	xmlNodePtr cur;
+
+	doc = xmlParseFile(docname);
+	
+	if (doc == NULL ) {
+		fprintf(stderr,"Document not parsed successfully. \n");
+		return;
+	}
+	
+	cur = xmlDocGetRootElement(doc);
+	
+	if (cur == NULL) {
+		fprintf(stderr,"empty document\n");
+		xmlFreeDoc(doc);
+		return;
+	}
+	
+	if (xmlStrcmp(cur->name, (const xmlChar *) "story")) {
+		fprintf(stderr,"document of the wrong type, root node != story");
+		xmlFreeDoc(doc);
+		return;
+	}
+	
+	getReference (doc, cur);
+	xmlFreeDoc(doc);
+	return;
+}
+
+int main(int argc, char **argv) {
+
+	const char *docname;
+
+	docname = "./variables.xml";
+	parseDoc (docname);
+	
+	return (1);
+}
