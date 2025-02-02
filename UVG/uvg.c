eceb211ec0ee0f4b0a5ea44a17df5bcb375415b8
RikyPlaza <rikyrod2001@gmail.com>
2025-02-02 13:43:19 +0000

    UVG/UVG.c: removed all xml parsing code
    Signed-off-by: RikyPlaza <rikyrod2001@gmail.com>

--- UVG/uvg.c
+++ UVG/uvg.c
@@ -1,46 +1,4 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <libxml/xmlmemory.h>
-#include <libxml/parser.h>
-
-void getReference(xmlDocPtr doc, xmlNodePtr cur) {
-
-	cur = cur->xmlChildrenNode;
-	while (cur != NULL) {
-	    if ((!xmlStrcmp(cur->name, (const xmlChar *)"devices"))) {
-		    printf("uri: %s\n", xmlGetProp(cur, "device"));
-		    }
-	    cur = cur->next;
-	}
-    return;
-}
-
-
-void parseDoc(char *docname) {
-
-	xmlDocPtr doc;
-	xmlNodePtr cur;
-
-	doc = xmlParseFile(docname);
-	
-	if (doc == NULL ) {
-		fprintf(stderr,"Document not parsed successfully. \n");
-		return;
-	}
-	
-	cur = xmlDocGetRootElement(doc);
-	
-	if (cur == NULL) {
-		fprintf(stderr,"empty document\n");
-		xmlFreeDoc(doc);
-		return;
-	}
-
-	getReference (doc, cur);
-	xmlFreeDoc(doc);
-	return;
-}
 
 int main(int argc, char **argv) {
 
