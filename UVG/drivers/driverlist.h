e4e6829abe0925c8ed6460084f34f9aaaeb17245
RikyPlaza <rikyrod2001@gmail.com>
2025-02-09 13:43:29 +0000

    UVG/drivers/driverlist.h: renamed constants
    Signed-off-by: RikyPlaza <rikyrod2001@gmail.com>

--- UVG/drivers/driverlist.h
+++ UVG/drivers/driverlist.h
@@ -1,3 +1,3 @@
-#define SIEMENS_CLIENT "siemens_client"
-#define OMRON_CLIENT "omron_client"
-#define OPCUA_CLIENT "opcua_client"
+#define SIEMENS_CLIENT_DESC "siemens_client"
+#define OMRON_CLIENT_DESC "omron_client"
+#define OPCUA_CLIENT_DESC "opcua_client"
