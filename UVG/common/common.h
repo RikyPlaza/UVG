1172b12add51a0cfa459f488b41ea818e7a50d17
RikyPlaza <rikyrod2001@gmail.com>
2025-02-09 14:30:39 +0000

    UVG/common/common.h UVG/common.structs.h
    moved max constants defines
    
    Signed-off-by: RikyPlaza <rikyrod2001@gmail.com>

--- UVG/common/common.h
+++ UVG/common/common.h
@@ -2,10 +2,6 @@
 
 #include "structs.h"
 
-#define CONFIG_PATH_MAX_LENGHT 512
-#define VAR_NUMBER_MAX 2048
-#define DEV_NUMBER_MAX 30
-
 const char* confpath(void);
 void initStructs(devices* devlist, variables* varlist);
 void populateStructs(devices* devlist, variables* varlist);
--- UVG/common/structs.h
+++ UVG/common/structs.h
@@ -1,3 +1,7 @@
+#define CONFIG_PATH_MAX_LENGHT 512
+#define VAR_NUMBER_MAX 2048
+#define DEV_NUMBER_MAX 30
+
 typedef enum
 {
     SIEMENS_CLIENT,
