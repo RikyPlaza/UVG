f94f9b3cc99c4d187d44815f7d0e03a792058be2
RikyPlaza <rikyrod2001@gmail.com>
2025-02-09 15:39:16 +0000

    General refactor x7
    Signed-off-by: RikyPlaza <rikyrod2001@gmail.com>

--- UVG/common/common.c
+++ UVG/common/common.c
@@ -1,8 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include "common.h"
-#include "strings/uvgstrings.h"
-#include "../drivers/driverlist.h"
 
 const char* confpath(void)
 {
--- UVG/common/xmlparser/xmlreader.h
+++ UVG/common/xmlparser/xmlreader.h
@@ -2,6 +2,8 @@
 #include <libxml/xmlmemory.h>
 #include <libxml/parser.h>
 #include "../structs.h"
+#include "strings/uvgstrings.h"
+#include "../drivers/driverlist.h"
 
 devtype getDevType(char* type);
 void getDevicesFromConfig(xmlDocPtr doc, xmlNodePtr cur, devices* devlist);
