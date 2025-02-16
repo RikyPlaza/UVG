0a670a362919a64c9e0c1951333f77cc436a748a
RikyPlaza <rikyrod2001@gmail.com>
2025-02-16 13:31:14 +0000

    UVG/uvg.c: called init function for logger
    Signed-off-by: RikyPlaza <rikyrod2001@gmail.com>

--- UVG/uvg.c
+++ UVG/uvg.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "common/common.h"
+#include "common/logger/uvglog.h"
 
 int main(int argc, char **argv) 
 {
 	devices devList;
 	variables varList;
-
+	
+	initlog();
 	initStructs(&devList, &varList);
 	populateStructs(&devList, &varList);
 	
