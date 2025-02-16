1fd3547b2213512b7cabc0d8b1ad828bb2e9893d
RikyPlaza <rikyrod2001@gmail.com>
2025-02-16 14:01:52 +0000

    UVG/common/logger/uvglog.h: corrected uvglog signature
    Signed-off-by: RikyPlaza <rikyrod2001@gmail.com>

--- UVG/common/logger/uvglog.h
+++ UVG/common/logger/uvglog.h
@@ -1,5 +1,5 @@
 time_t clk;
 
 void initlog();
-void uvglog(const char* log, int codeLine, const char* funcName);
+void uvglog(const char* log);
 void uvgdebuglog(const char* log, int codeLine, const char* funcName);
