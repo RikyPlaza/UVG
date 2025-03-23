f4ebcbf4027e6bc6f3a049a0a2538c955d44ad70
RikyPlaza <rikyrod2001@gmail.com>
2025-03-23 15:16:28 +0000

    UVG/common/strings/uvgstring.h: defined max string lenght
    Signed-off-by: RikyPlaza <rikyrod2001@gmail.com>

--- UVG/common/strings/uvgstrings.h
+++ UVG/common/strings/uvgstrings.h
@@ -2,6 +2,8 @@
 #include <ctype.h>
 #include <stdarg.h>
 
+#define STRINGLEN 1024
+
 char* strtolower(char* str);
-char* uvgsprintf(char* buf, char* fmt, ...);
+char* uvgsprintf(char* fmt, ...);
 char* uvgremovechar(char* buf, char ch);
